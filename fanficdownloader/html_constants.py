4c99fdbcc11cd71cbc3f07bc1ada101a8b11b3bb
wsuetholz <wsuetholz@localhost>
2010-11-30 15:17:00 -0600

    Removed some leftover functions that were left from using ffnet.py as a base for fpcom.py and mediaminer.py.

---
+++ fanficdownloader/html_constants.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+XHTML_START = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>${title} by ${author}</title>
+<link href="stylesheet.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+<div>
+<h1>${title} by ${author}</h1>
+${body}
+</body></html>
+'''
+
+XHTML_CHAPTER_START = '''<h2>${chapter}</h2>'''
+
+XHTML_END = ''''''
