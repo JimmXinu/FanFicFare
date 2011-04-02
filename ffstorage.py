302a8f12a3bb2ee5e71f7e45c9d1b2d7febecce5
Jim Miller <retiefjimm@gmail.com>
2011-04-01 22:14:05 -0500

    Removing storage for old version.  Fixes for whofic meta data.

--- ffstorage.py
+++ ffstorage.py
@@ -1,25 +1,5 @@
 from google.appengine.ext import db
 
-class OneDownload(db.Model):
-	user = db.UserProperty()
-	url = db.StringProperty()
-	format = db.StringProperty()
-	login = db.StringProperty()
-	password = db.StringProperty()
-	failure = db.StringProperty()
-	date = db.DateTimeProperty(auto_now_add=True)
-	
-class DownloadedFanfic(db.Model):
-	user = db.UserProperty()
-	url = db.StringProperty()
-	name = db.StringProperty()
-	author = db.StringProperty()
-	format = db.StringProperty()
-	date = db.DateTimeProperty(auto_now_add=True)
-	blob = db.BlobProperty()
-	mac = db.StringProperty()
-	cleared = db.BooleanProperty(default=False)
-
 class DownloadMeta(db.Model):
 	user = db.UserProperty()
 	url = db.StringProperty()
