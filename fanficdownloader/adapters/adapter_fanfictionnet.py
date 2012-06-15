938cb4eec5bf4fd1a1f84f5237e56049c49674cd
Ida <Ida@Iatheia>
2012-06-14 20:57:30 -0400

    First version of the adapters for yourfanfiction.com and samdean.archive.nu

---
+++ fanficdownloader/adapters/adapter_fanfictionnet.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2011 Fanficdownloader team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import time
+import logging
+import re
+import urllib2
+import time
+
+from .. import BeautifulSoup as bs
+from .. import exceptions as exceptions
+from ..htmlcleanup import stripHTML
+
+from base_adapter import BaseSiteAdapter,  makeDate
+
+ffnetgenres=["Adventure", "Angst", "Crime", "Drama", "Family", "Fantasy", "Friendship", "General",
+             "Horror", "Humor", "Hurt-Comfort", "Mystery", "Parody", "Poetry", "Romance", "Sci-Fi",
+             "Spiritual", "Supernatural", "Suspense", "Tragedy", "Western"]
+
+class FanFictionNetSiteAdapter(BaseSiteAdapter):
+
+    def __init__(self, config, url):
+        BaseSiteAdapter.__init__(self, config, url)
+        self.story.setMetadata('siteabbrev','ffnet')
+        
+        # get storyId from url--url validation guarantees second part is storyId
+        self.story.setMetadata('storyId',self.parsedUrl.path.split('/',)[2])
+
+        # normalized story URL.
+        self._setURL("http://"+self.getSiteDomain()\
+                         +"/s/"+self.story.getMetadata('storyId')+"/1/")
+
+        # ffnet update emails have the latest chapter URL.
+        # Frequently, when they arrive, not all the servers have the
+        # latest chapter yet and going back to chapter 1 to pull the
+        # chapter list doesn't get the latest.  So save and use the
+        # original URL given to pull chapter list & metadata.
+        self.origurl = url
+        if "http://m." in self.origurl:
+            ## accept m(mobile)url, but use www.
+            self.origurl = self.origurl.replace("http://m.","http://www.")
+
+    @staticmethod
+    def getSiteDomain():
+        return 'www.fanfiction.net'
+
+    @classmethod
+    def getAcceptDomains(cls):
+        return ['www.fanfiction.net','m.fanfiction.net']
+
+    def getSiteExampleURLs(self):
+        return "http://www.fanfiction.net/s/1234/1/ http://www.fanfiction.net/s/1234/12/ http://www.fanfiction.net/s/1234/1/Story_Title"
+
+    def getSiteURLPattern(self):
+        return r"http://(www|m)?\.fanfiction\.net/s/\d+(/\d+)?(/|/[a-zA-Z0-9_-]+)?/?$"
+
+    def extractChapterUrlsAndMetadata(self):
+
+        # fetch the chapter.  From that we will get almost all the
+        # metadata and chapter list
+
+        url = self.origurl
+        logging.debug("URL: "+url)
+
+        # use BeautifulSoup HTML parser to make everything easier to find.
+        try:
+            data = self._fetchUrl(url)
+            #print("\n===================\n%s\n===================\n"%data)
+            soup = bs.BeautifulSoup(data)
+        except urllib2.HTTPError, e:
+            if e.code == 404:
+                raise exceptions.StoryDoesNotExist(url)
+            else:
+                raise e
+            
+        if "Unable to locate story with id of " in data:
+            raise exceptions.StoryDoesNotExist(url)
+
+        # some times "Chapter not found...", sometimes "Chapter text not found..."
+        if "not found. Please check to see you are not using an outdated url." in data:
+            raise exceptions.FailedToDownload("Error downloading Chapter: %s!  'Chapter not found. Please check to see you are not using an outdated url.'" % url)
+
+        try:
+            # rather nasty way to check for a newer chapter.  ffnet has a
+            # tendency to send out update notices in email before all
+            # their servers are showing the update on the first chapter.
+            try:
+                chapcount = len(soup.find('select', { 'name' : 'chapter' } ).findAll('option'))
+            # get chapter part of url.
+            except:
+                chapcount = 1
+            chapter = url.split('/',)[5]
+            tryurl = "http://%s/s/%s/%d/"%(self.getSiteDomain(),
+                                           self.story.getMetadata('storyId'),
+                                           chapcount+1)
+            print('=Trying newer chapter: %s' % tryurl)
+            newdata = self._fetchUrl(tryurl)
+            if "not found. Please check to see you are not using an outdated url." \
+                    not in newdata:
+                print('=======Found newer chapter: %s' % tryurl)
+                soup = bs.BeautifulSoup(newdata)
+        except:
+            pass
+        
+        # Find authorid and URL from... author url.
+        a = soup.find('a', href=re.compile(r"^/u/\d+"))
+        self.story.setMetadata('authorId',a['href'].split('/')[2])
+        self.story.setMetadata('authorUrl','http://'+self.host+a['href'])
+        self.story.setMetadata('author',a.string)
+
+            
+        # start by finding a script towards the bottom that has a
+        # bunch of useful stuff in it.
+            
+        # var storyid = 6577076;
+        # var chapter = 1;
+        # var chapters = 17;
+        # var words = 42787;
+        # var userid = 2645830;
+        # var title = 'The+Invitation';
+        # var title_t = 'The Invitation';
+        # var summary = 'Dudley Dursley would be the first to say he lived a very normal life. But what happens when he gets invited to his cousin Harry Potter\'s wedding? Will Dudley get the courage to apologize for the torture he caused all those years ago? Harry/Ginny story.';
+        # var categoryid = 224;
+        # var cat_title = 'Harry Potter';
+        # var datep = '12-21-10';
+        # var dateu = '04-06-11';
+        # var author = 'U n F a b u l o u s M e';
+
+        for script in soup.findAll('script', src=None):
+            if not script:
+                continue
+            if not script.string:
+                continue
+            if 'var storyid' in script.string:
+                for line in script.string.split('\n'):
+                    m = re.match(r"^ +var ([^ ]+) = '?(.*?)'?;\r?$",line)
+                    if m == None : continue
+                    var,value = m.groups()
+                    # remove javascript escaping from values.
+                    value = re.sub(r'\\(.)',r'\1',value)
+                    #print var,value
+                    if 'words' in var:
+                        self.story.setMetadata('numWords', value)
+                    if 'title_t' in var:
+                        self.story.setMetadata('title', value)
+                    if 'summary' in var:
+                        self.setDescription(url,value)
+                        #self.story.setMetadata('description', value)
+                    if 'datep' in var:
+                        self.story.setMetadata('datePublished',makeDate(value, '%m-%d-%y'))
+                    if 'dateu' in var:
+                        self.story.setMetadata('dateUpdated',makeDate(value, '%m-%d-%y'))
+                    if 'cat_title' in var:
+                        if "Crossover" in value:
+                            value = re.sub(r' Crossover$','',value)
+                            for c in value.split(' and '):
+                                self.story.addToList('category',c)
+                                # Screws up when the category itself
+                                # contains ' and '.  But that's rare
+                                # and the only alternative is to find
+                                # the 'Crossover' category URL and
+                                # parse that page to search for <a>
+                                # with href /crossovers/(name)/(num)/
+				# <a href="/crossovers/Harry_Potter/224/">Harry Potter</a>
+				# <a href="/crossovers/Naruto/1402/">Naruto</a>
+                        else:
+                            self.story.addToList('category',value)
+                break # for script in soup.findAll('script', src=None):
+            
+        # Find the chapter selector 
+        select = soup.find('select', { 'name' : 'chapter' } )
+    	 
+        if select is None:
+    	   # no selector found, so it's a one-chapter story.
+    	   self.chapterUrls.append((self.story.getMetadata('title'),url))
+        else:
+            allOptions = select.findAll('option')
+            for o in allOptions:
+                url = u'http://%s/s/%s/%s/' % ( self.getSiteDomain(),
+                                            self.story.getMetadata('storyId'),
+                                            o['value'])
+                # just in case there's tags, like <i> in chapter titles.
+                title = u"%s" % o
+                title = re.sub(r'<[^>]+>','',title)
+                self.chapterUrls.append((title,url))
+
+        self.story.setMetadata('numChapters',len(self.chapterUrls))
+
+        ## Pull some additional data from html.  Find Rating and look around it.
+
+        a = soup.find('a', href='http://www.fictionratings.com/')
+        rating = a.string
+        if 'Fiction' in rating: # if rating has 'Fiction ', strip that out for consistency with past.
+            rating = rating[8:]
+            
+        self.story.setMetadata('rating',rating)
+
+        # after Rating, the same bit of text containing id:123456 contains
+        # Complete--if completed.
+        gui_table1i = soup.find(id="gui_table1i")
+        metatext = stripHTML(gui_table1i.find('div', {'style':'color:gray;'})).replace('Hurt/Comfort','Hurt-Comfort')
+        metalist = metatext.split(" - ")
+        #print("metatext:(%s)"%metalist)
+
+        # rating is obtained above more robustly.
+        if metalist[0].startswith('Rated:'):
+            metalist=metalist[1:]
+
+        # next is assumed to be language.
+        self.story.setMetadata('language',metalist[0])
+        metalist=metalist[1:]
+
+        # next might be genre.
+        genrelist = metalist[0].split('/') # Hurt/Comfort already changed above.
+        goodgenres=True
+        for g in genrelist:
+            if g not in ffnetgenres:
+                goodgenres=False
+        if goodgenres:
+            self.story.extendList('genre',genrelist)
+            metalist=metalist[1:]
+
+        # next might be characters, otherwise Reviews, Updated or Published
+        if not ( metalist[0].startswith('Reviews') or metalist[0].startswith('Updated') or metalist[0].startswith('Published') ):
+            self.story.extendList('characters',metalist[0].split(' & '))        
+        
+        if 'Status: Complete' in metatext:
+            self.story.setMetadata('status', 'Completed')
+        else:
+            self.story.setMetadata('status', 'In-Progress')
+        return
+
+    def getChapterText(self, url):
+        logging.debug('Getting chapter text from: %s' % url)
+        time.sleep(0.5) ## ffnet(and, I assume, fpcom) tends to fail
+                        ## more if hit too fast.  This is in
+                        ## additional to what ever the
+                        ## slow_down_sleep_time setting is.
+        data = self._fetchUrl(url)
+        soup = bs.BeautifulSoup(data)
+
+        ## Remove the 'share' button.
+        sharediv = soup.find('div', {'class' : 'a2a_kit a2a_default_style'})
+        if sharediv:
+            sharediv.extract()
+        
+        div = soup.find('div', {'id' : 'storytextp'})
+        
+        if None == div:
+            logging.debug('div id=storytextp not found.  data:%s'%data)
+            raise exceptions.FailedToDownload("Error downloading Chapter: %s!  Missing required element!" % url)
+
+        return self.utf8FromSoup(url,div)
+
+def getClass():
+    return FanFictionNetSiteAdapter
+
---
+++ fanficdownloader/adapters/adapter_fimfictionnet.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2011 Fanficdownloader team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import time
+import logging
+import re
+import urllib2
+import cookielib as cl
+from datetime import datetime
+import json
+
+from .. import BeautifulSoup as bs
+from ..htmlcleanup import stripHTML
+from .. import exceptions as exceptions
+
+from base_adapter import BaseSiteAdapter,  makeDate
+
+def getClass():
+    return FimFictionNetSiteAdapter
+
+class FimFictionNetSiteAdapter(BaseSiteAdapter):
+    
+    def __init__(self, config, url):
+        BaseSiteAdapter.__init__(self, config, url)
+        self.story.setMetadata('siteabbrev','fimficnet')
+        self.story.setMetadata('storyId', self.parsedUrl.path.split('/',)[2])
+        self._setURL("http://"+self.getSiteDomain()+"/story/"+self.story.getMetadata('storyId')+"/")
+        self.is_adult = False
+        
+    @staticmethod
+    def getSiteDomain():
+        return 'www.fimfiction.net'
+
+    @classmethod
+    def getAcceptDomains(cls):
+        # mobile.fimifction.com isn't actually a valid domain, but we can still get the story id from URLs anyway
+        return ['www.fimfiction.net','mobile.fimfiction.net', 'www.fimfiction.com', 'mobile.fimfiction.com']
+
+    def getSiteExampleURLs(self):
+        return "http://www.fimfiction.net/story/1234/story-title-here http://www.fimfiction.net/story/1234/ http://www.fimfiction.com/story/1234/1/ http://mobile.fimfiction.net/story/1234/1/story-title-here/chapter-title-here"
+
+    def getSiteURLPattern(self):
+        return r"http://(www|mobile)\.fimfiction\.(net|com)/story/\d+/?.*"
+        
+    def extractChapterUrlsAndMetadata(self):
+        
+        if self.is_adult or self.getConfig("is_adult"):
+            cookieproc = urllib2.HTTPCookieProcessor()
+            cookie = cl.Cookie(version=0, name='view_mature', value='true',
+                               port=None, port_specified=False,
+                               domain=self.getSiteDomain(), domain_specified=False, domain_initial_dot=False,
+                               path='/story', path_specified=True,
+                               secure=False,
+                               expires=time.time()+10000,
+                               discard=False,
+                               comment=None,
+                               comment_url=None,
+                               rest={'HttpOnly': None},
+                               rfc2109=False)
+            cookieproc.cookiejar.set_cookie(cookie)
+            self.opener = urllib2.build_opener(cookieproc)
+        
+        try:
+            apiResponse = urllib2.urlopen("http://www.fimfiction.net/api/story.php?story=%s" % (self.story.getMetadata("storyId"))).read()
+            apiData = json.loads(apiResponse)
+            
+            # Unfortunately, we still need to load the story index page to parse the characters
+            data = self._fetchUrl(self.url)
+        except urllib2.HTTPError, e:
+            if e.code == 404:
+                raise exceptions.StoryDoesNotExist(self.url)
+            else:
+                raise e
+        
+        if "Warning: mysql_fetch_array(): supplied argument is not a valid MySQL result resource" in data:
+            raise exceptions.StoryDoesNotExist(self.url)
+
+        if "/images/missing_story.png" in data:
+            raise exceptions.StoryDoesNotExist(self.url)
+        
+        if "Invalid story id" in apiData.values():
+            raise exceptions.StoryDoesNotExist(self.url)
+        
+        if "This story has been marked as having adult content." in data:
+            raise exceptions.AdultCheckRequired(self.url)
+        
+        if self.password:
+            params = {}
+            params['password'] = self.password
+            data = self._postUrl(self.url,params)
+
+        if "Enter the password the author set for this story to view it." in data:
+            if self.getConfig('fail_on_password'):
+                raise exceptions.FailedToDownload("%s requires story password and fail_on_password is true."%self.url)
+            else:
+                raise exceptions.FailedToLogin(self.url,"Story requires individual password",passwdonly=True)
+         
+        storyMetadata = apiData["story"]    
+            
+        self.story.setMetadata("title", storyMetadata["title"])
+        self.story.setMetadata("author", storyMetadata["author"]["name"])
+        self.story.setMetadata("authorId", storyMetadata["author"]["id"])
+        self.story.setMetadata("authorUrl", "http://%s/user/%s" % (self.getSiteDomain(), storyMetadata["author"]["name"]))
+        
+        chapters = [{"chapterTitle": chapter["title"], "chapterURL": chapter["link"]} for chapter in storyMetadata["chapters"]]
+        for chapter in chapters:
+            self.chapterUrls.append((chapter["chapterTitle"], chapter["chapterURL"]))
+        self.story.setMetadata("numChapters", len(self.chapterUrls))
+        
+        # In the case of fimfiction.net, possible statuses are 'Completed', 'Incomplete', 'On Hiatus' and 'Cancelled'
+        # For the sake of bringing it in line with the other adapters, 'Incomplete' and 'On Hiatus' become 'In-Progress'
+        # and 'Complete' beomes 'Completed'. 'Cancelled' seems an important enough (not to mention more strictly true) 
+        # status to leave unchanged.
+        status = storyMetadata["status"].replace("Incomplete", "In-Progress").replace("On Hiatus", "In-Progress").replace("Complete", "Completed")
+        self.story.setMetadata("status", status)
+        self.story.setMetadata("rating", storyMetadata["content_rating_text"])
+            
+        for category in storyMetadata["categories"]:
+            if storyMetadata["categories"][category]:
+                self.story.addToList("genre", category) 
+
+        self.story.addToList("category", "My Little Pony")
+        
+        self.story.setMetadata("numWords", str(storyMetadata["words"]))
+        
+        
+        # fimfic is the first site with an explicit cover image.
+        if self.getConfig('include_images') and "image" in storyMetadata.keys():
+            coverurl = storyMetadata["image"]
+            if coverurl.startswith('//static.fimfiction.net'): # fix for img urls missing 'http:'
+                coverurl = "http:"+coverurl
+            self.story.addImgUrl(self,self.url,coverurl,self._fetchUrlRaw,cover=True)
+            
+        self.setDescription(self.url, storyMetadata["description"])
+        
+        # Dates are in Unix time
+        # Take the publish date from the first chapter posted
+        rawDatePublished = storyMetadata["chapters"][0]["date_modified"]
+        self.story.setMetadata("datePublished", datetime.fromtimestamp(rawDatePublished))
+        rawDateUpdated = storyMetadata["date_modified"]
+        self.story.setMetadata("dateUpdated", datetime.fromtimestamp(rawDateUpdated))
+        
+        soup = bs.BeautifulSoup(data).find("div", {"class":"story"})
+        for character in [character_icon["title"] for character_icon in soup.findAll("a", {"class":"character_icon"})]:
+            self.story.addToList("characters", character)
+            
+            
+    def getChapterText(self, url):
+        logging.debug('Getting chapter text from: %s' % url)
+        soup = bs.BeautifulSoup(self._fetchUrl(url),selfClosingTags=('br','hr')).find('div', {'id' : 'chapter_container'})
+        if soup == None:
+            raise exceptions.FailedToDownload("Error downloading Chapter: %s!  Missing required element!" % url)
+        return self.utf8FromSoup(url,soup)
+        
