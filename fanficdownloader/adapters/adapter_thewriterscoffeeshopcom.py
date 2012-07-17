71ebc394ae84243f677e449fced77435dc5c22bb
retiefjimm <retiefjimm@localhost>
2012-07-17 13:42:27 -0500

    thewriterscoffeeshopcom: stripHTML on title

--- fanficdownloader/adapters/adapter_thewriterscoffeeshopcom.py
+++ fanficdownloader/adapters/adapter_thewriterscoffeeshopcom.py
@@ -133,7 +133,7 @@ class TheWritersCoffeeShopComSiteAdapter(BaseSiteAdapter):
 
         ## Title
         a = soup.find('a', href=re.compile(r'viewstory.php\?sid='+self.story.getMetadata('storyId')+"$"))
-        self.story.setMetadata('title',a.string)
+        self.story.setMetadata('title',stripHTML(a))
         
         # Find authorid and URL from... author url.
         a = soup.find('a', href=re.compile(r"viewuser.php\?uid=\d+"))
