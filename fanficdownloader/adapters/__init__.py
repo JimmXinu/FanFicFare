1f2dc1a5cd0e52dc4a19e1c5052ff95485da02f5
retiefjimm <retiefjimm@localhost>
2013-10-11 15:04:21 -0500

    Adding fictionpad.com.  Doesn't support login and 18+ yet.

--- fanficdownloader/adapters/__init__.py
+++ fanficdownloader/adapters/__init__.py
@@ -119,6 +119,7 @@ import adapter_nickandgregnet
 import adapter_potterheadsanonymouscom
 import adapter_simplyundeniablecom
 import adapter_scarheadnet
+import adapter_fictionpadcom
 
 ## This bit of complexity allows adapters to be added by just adding
 ## importing.  It eliminates the long if/else clauses we used to need
