ad08ba172dcf64b9e935178e78c82007176c7f21
doe <doe5716@gmail.com>
2014-08-06 15:33:21 +0200

    Foundations of an eFiction base adapter * works for fannation and themaplebookshop * metadata parsing must be more extensible * missing documentation * proper handling of warnings / is_adult checks * ...

--- fanficdownloader/adapters/adapter_themaplebookshelf.py
+++ fanficdownloader/adapters/adapter_themaplebookshelf.py
@@ -15,116 +15,22 @@
 # limitations under the License.
 #
 
-import logging
-logger = logging.getLogger(__name__)
-import re
-import urllib2
+from base_efiction_adapter import BaseEfictionAdapter
 
-from .. import BeautifulSoup as bs
-from ..htmlcleanup import stripHTML
-from .. import exceptions as exceptions
-
-from base_adapter import BaseSiteAdapter,  makeDate
-
-
-class TheMapleBookshelfComSiteAdapter(BaseSiteAdapter):
-    """
-    Use Printable version which is easier to parse and has everything in one
-    page and cache between extractChapterUrlsAndMetadata and getChapterText
-    """
-
-    def __init__(self, config, url):
-        BaseSiteAdapter.__init__(self, config, url)
-        self.story.setMetadata('siteabbrev','maplebook')
-        self.decode = ["Windows-1252", "utf8"]
-        self.story.setMetadata('storyId', re.compile(self.getSiteURLPattern()).match(url).group('storyId'))
-        self._setURL("http://%s/Literati/viewstory.php?sid=%s" % (self.getSiteDomain(), self.story.getMetadata('storyId')))
-        self.dateformat = "%b %d, %Y"
+class TheMapleBookshelfComSiteAdapter(BaseEfictionAdapter):
 
     @staticmethod
     def getSiteDomain():
         return 'themaplebookshelf.com'
 
-    @classmethod
-    def getSiteExampleURLs(self):
-        return "http://www.themaplebookshelf.com/Literati/viewstory.php?sid=227 http://themaplebookshelf.com/Literati/viewstory.php?sid=227&chapter=2"
+    def getPathToArchive(self):
+        return '/Literati'
 
-    def getSiteURLPattern(self):
-        return r"http://themaplebookshelf.com/Literati/viewstory.php\?sid=(?P<storyId>\d+)"
+    def getSiteAbbrev(seluuf):
+        return 'maplebook'
 
-    def extractChapterUrlsAndMetadata(self):
-        logger.debug(self.url)
-        self._setURL(self.url + "&action=printable")
-        try:
-            html = self._fetchUrl(self.url)
-            soup = bs.BeautifulSoup(html)
-            # #strip comments from soup
-            # [comment.extract() for comment in soup1.findAll(text=lambda text:isinstance(text, bs.Comment))]
-        except urllib2.HTTPError, e:
-            if e.code == 404:
-                raise exceptions.StoryDoesNotExist(self.url)
-            else:
-                raise e
-        ## title + author
-        pagetitleDiv = soup.find("div", {"id": "pagetitle"})
-        self.story.setMetadata('title', pagetitleDiv.find("a").text)
-        authorLink = pagetitleDiv.findAll("a")[1]
-        self.story.setMetadata('author', authorLink.text)
-        self.story.setMetadata('authorUrl', "http://" + self.getSiteDomain() + "/" + authorLink['href'])
-        self.story.setMetadata('authorId', re.search("\d+", authorLink['href']).group(0))
-
-        ## Description
-        description = ""
-        summaryEnd = soup.find("div", "content").find("span", "label").nextSibling
-        while summaryEnd is not None:
-            description += stripHTML(summaryEnd)
-            summaryEnd = summaryEnd.nextSibling
-            if type(summaryEnd) != bs.NavigableString and summaryEnd.name == 'br':
-                break
-        self.story.setMetadata('description', description)
-
-        ## General Metadata
-        for kSpan in soup.findAll("span", "label"):
-            k = kSpan.text.strip().replace(':', '')
-            vSpan = kSpan.nextSibling
-            if k == 'Summary:' or not vSpan or not vSpan.string:
-                continue
-            v = vSpan.string.strip()
-            if v == 'None':
-                continue
-            logger.debug("%s '%s'" %(k, v))
-            if k == 'Genre':
-                for genre in v.split(", "):
-                    self.story.addToList('genre', genre)
-            elif k == 'Chapters':
-                self.story.setMetadata('numChapters', int(v))
-            elif k == 'Word count':
-                self.story.setMetadata('numWords', v)
-            elif k == 'Published':
-                self.story.setMetadata('datePublished', makeDate(v, self.dateformat))
-            elif k == 'Updated':
-                self.story.setMetadata('dateUpdated', makeDate(v, self.dateformat))
-            # TODO: Series, Warnings
-
-        ## Chapter URLs (fragment identifiers in the document, so we don' need to fetch so much)
-        for chapterNumB in soup.findAll("b", text=re.compile("^\d+\.$")):
-            self.chapterUrls.append((
-                chapterNumB.parent.parent.find("a").text,
-                self.url + chapterNumB.parent.parent.find("a")["href"]
-                ))
-
-        ## fix all local image 'src' to absolute
-        for img in soup.findAll("img", {"src": re.compile("^(?!http)")}):
-            img['src'] = re.sub("viewstory.php\?.*", "", self.url) + img['src']
-
-        self.html = soup
-
-    def getChapterText(self, url):
-        logger.debug('Getting chapter text from <%s>' % url)
-        anchor = url.replace(self.url, "")
-        anchor = anchor.replace("#", "")
-        chapterDiv = self.html.find("a", {"name": anchor}).parent.findNext("div", "chapter")
-        return self.utf8FromSoup(self.url, chapterDiv)
+    def getDateFormat(self):
+        return "%b %d, %Y"
 
 def getClass():
     return TheMapleBookshelfComSiteAdapter
