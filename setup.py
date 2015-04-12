853295bcf9366aa998d602d38487b48647cf535b
Jim Miller <retiefjimm@gmail.com>
2015-04-12 17:28:12 -0500

    Switch to real pypi, still need '--pre' flag while beta.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="2.2.0.a1",
+    version="2.2.0.b1",
 
     description='A tool for downloading fanfiction to eBook formats',
     long_description=long_description,
@@ -46,7 +46,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         'Environment :: Console',
 
@@ -60,7 +60,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.2',
