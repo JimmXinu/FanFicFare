a84c5dea191f9dc812084ebb79118f663d4967a5
Jim Miller <retiefjimm@gmail.com>
2015-06-30 22:44:06 -0500

    Remove outdated comment.

--- fanficfare/writers/writer_epub.py
+++ fanficfare/writers/writer_epub.py
@@ -661,10 +661,6 @@ div { margin: 0pt; padding: 0pt; }
                 # as one line.  This causes problems for nook(at
                 # least) when the chapter size starts getting big
                 # (200k+)
-                #fullhtml = fullhtml.replace('</p>','</p>\n').replace('<br />','<br />\n')
-                # The replaces above added tons of extra newlines
-                # during *each* epub update.  The regexp version adds
-                # only one and removes any extra.
                 fullhtml = re.sub(r'(</p>|<br ?/>)\n*',r'\1\n',fullhtml)
 
                 outputepub.writestr("OEBPS/file%04d.xhtml"%(index+1),fullhtml.encode('utf-8'))
