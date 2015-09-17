f181ff2c03737680b66251ccb806179f9d94f582
Jim Miller <retiefjimm@gmail.com>
2015-09-17 10:50:46 -0500

    Bump versions

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="2.2.12",
+    version="2.2.13",
 
     description='A tool for downloading fanfiction to eBook formats',
     long_description=long_description,
