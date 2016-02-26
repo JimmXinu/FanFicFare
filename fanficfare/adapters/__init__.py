2f0e431e35ef0fb72eb2978c9ebd5a31ef501da9
Jim Miller <retiefjimm@gmail.com>
2016-02-25 19:06:15 -0600

    Fix adapters list for PR 117.

--- fanficfare/adapters/__init__.py
+++ fanficfare/adapters/__init__.py
@@ -139,10 +139,11 @@ import adapter_ninelivesarchivecom
 import adapter_masseffect2in
 import adapter_quotevcom
 import adapter_mcstoriescom
+
 import adapter_lucifaelff
 import adapter_buffygilescom
-import adapter_rubyquillcom
-#import adapter_andromedawebcom # Not all lables are captured
+#import adapter_rubyquillcom
+import adapter_andromedawebcom # Not all lables are captured
 import adapter_artemisfowlcom
 import adapter_rabidreadercom
 import adapter_naiceanilmenet
