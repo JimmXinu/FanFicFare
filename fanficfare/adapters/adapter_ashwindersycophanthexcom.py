6f7e700cd24fbf6d2c7881c7a544de6d501d6b08
Jim Miller <retiefjimm@gmail.com>
2016-05-19 09:13:18 -0500

    Fix for adapter_ashwindersycophanthexcom login.

--- fanficfare/adapters/adapter_ashwindersycophanthexcom.py
+++ fanficfare/adapters/adapter_ashwindersycophanthexcom.py
@@ -77,7 +77,8 @@ class AshwinderSycophantHexComAdapter(BaseSiteAdapter):
     ## Login seems to be reasonably standard across eFiction sites.
     def needToLoginCheck(self, data):
         if 'This story contains adult content and/or themes.' in data \
-                or "That password doesn't match the one in our database" in data:
+                or "That password doesn't match the one in our database" in data \
+                or "Member Login" in data:
             return True
         else:
             return False
