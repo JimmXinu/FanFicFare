a0b4332da8d78d39a0b62492ebcdd8575ca66b66
M Clark <wassname@users.noreply.github.com>
2016-10-28 15:07:25 +0800

    better description selector

--- fanficfare/adapters/adapter_royalroadl.py
+++ fanficfare/adapters/adapter_royalroadl.py
@@ -132,7 +132,7 @@ class RoyalRoadAdapter(BaseSiteAdapter):
         self.story.setMetadata('numChapters',len(self.chapterUrls))
 
         # this is forum based so it's a bit ugly
-        description=soup.find('div',{'class':'description'})
+        description = soup.find('div', {'property': 'description', 'class': 'hidden-content'})
         self.setDescription(url,description)
 
         dates = [tr.findAll('td')[1] for tr in chapters.findAll('tr')]
