0d1165259d682540226b38bfa868f9c4d2177320
Jim Miller <retiefjimm@gmail.com>
2017-01-21 17:10:06 -0600

    Bump test version.

--- calibre-plugin/__init__.py
+++ calibre-plugin/__init__.py
@@ -48,7 +48,7 @@ class FanFicFareBase(InterfaceActionBase):
     description         = _('UI plugin to download FanFiction stories from various sites.')
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Jim Miller'
-    version             = (2, 8, 4)
+    version             = (2, 8, 5)
     minimum_calibre_version = (1, 48, 0)
 
     #: This field defines the GUI plugin class that contains all the code
