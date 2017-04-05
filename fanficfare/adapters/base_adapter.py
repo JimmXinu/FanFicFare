a7d5f355656ac3c68622c687201b26a9f54414c7
Jim Miller <retiefjimm@gmail.com>
2017-04-05 15:35:45 -0500

    Allow fractional series num in calibre injected series.

--- fanficfare/adapters/base_adapter.py
+++ fanficfare/adapters/base_adapter.py
@@ -294,7 +294,11 @@ class BaseSiteAdapter(Configurable):
     # Just for series, in case we choose to change how it's stored or represented later.
     def setSeries(self,name,num):
         if self.getConfig('collect_series'):
-            self.story.setMetadata('series','%s [%s]'%(name, int(num)))
+            ## fractional series can come from calibre injected series.
+            num = float(num)
+            if num.is_integer():
+                num = int(num)
+            self.story.setMetadata('series','%s [%s]'%(name, num))
 
     def setDescription(self,url,svalue):
         #print("\n\nsvalue:\n%s\n"%svalue)
