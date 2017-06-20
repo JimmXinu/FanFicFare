24c0304aa53b4c45deec4f9e6db78121ad3851da
Jim Miller <retiefjimm@gmail.com>
2017-06-20 14:43:27 -0500

    Bump Test Version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="FanFicFare",
 
     # Versions should comply with PEP440.
-    version="2.12.16",
+    version="2.12.17",
 
     description='A tool for downloading fanfiction to eBook formats',
     long_description=long_description,
