d06ef4bd572d191f168917be2be83fa93d3b47c4
Dmitry Snegirev <rikkitp@gmail.com>
2018-04-08 20:22:52 +0300

    Add reformating to lightnovelgate

--- fanficfare/adapters/adapter_lightnovelgatecom.py
+++ fanficfare/adapters/adapter_lightnovelgatecom.py
@@ -26,10 +26,23 @@ import urlparse
 
 from base_adapter import BaseSiteAdapter, makeDate
 
-from ..htmlcleanup import stripHTML
+from bs4 import Comment
+from ..htmlcleanup import removeEntities, stripHTML
 from .. import exceptions as exceptions
 
 logger = logging.getLogger(__name__)
+HTML_TAGS = (
+    'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi',
+    'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col',
+    'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed',
+    'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5',
+    'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link',
+    'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol',
+    'optgroup', 'option', 'output', 'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp',
+    'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup',
+    'svg', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt',
+    'u', 'ul', 'var', 'video', 'wbr')
+
 
 def getClass():
     ''' Initializing the class '''
@@ -150,12 +163,44 @@ class LightNovelGateSiteAdapter(BaseSiteAdapter):
         cdata.find('h2').extract()
         self.setDescription(url, cdata)
 
+    def fixExcessSpace(self, data):
+        # For easier extra space removing (when combining p an br)
+        data = removeEntities(data)
+
+        # Sometimes we don't have even tags like <p> or <br/>, so lets create <p> instead of two new_line
+        data = re.sub(r"\n[ \s]*\n", "\n<p>", data, flags=re.UNICODE)
+
+        # Combining all consequence of p and br to one <p>
+        # bs4 will create </p> on his own, so don't worry
+        data = re.sub(r"[ \s]*(</?p\b[^>]*>[ \s]*|<br\b[^>]*>[ \s]*)+", "\n<p>", data, flags=re.UNICODE)
+
+        return data
+
     def getChapterText(self, url):
         data = self._fetchUrl(url)
+
+        # Sometimes we get invalid characters
+        data = data.decode('utf-8','ignore').encode('utf-8')
+
+        if self.getConfig('fix_excess_space', True):
+            data = self.fixExcessSpace(data)
+
         soup = self.make_soup(data)
+
         story = soup.find('div', {'id':'vung_doc'})
         if not story:
             raise exceptions.FailedToDownload(
                 "Error downloading Chapter: %s!  Missing required element!" % url)
+
+        # Some comments we will get is invalid. Remove them all.
+        [comment.extract() for comment in story.find_all(text=lambda text:isinstance(text, Comment))]
+
+        # We don't need links. They have a bad css and they are not working most of times.
+        [a.extract() for a in story.find_all('a')]
+
+        # Some tags have non-standard tag name.
+        for tag in story.findAll(recursive=True):
+            if tag.name not in HTML_TAGS:
+                tag.name = 'span'
 
         return self.utf8FromSoup(url, story)
