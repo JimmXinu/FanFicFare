0783a74b59325038b45ab2b1dc86621315ef92f9
Jim Miller <retiefjimm@gmail.com>
2018-07-26 17:41:54 -0500

    test1.com with epub/txt/html output working, mobi broken.

--- fanficfare/writers/writer_epub.py
+++ fanficfare/writers/writer_epub.py
@@ -581,9 +581,10 @@ div { margin: 0pt; padding: 0pt; }
         # write content.opf to zip.
         contentxml = contentdom.toxml(encoding='utf-8')
 
+        # Causes py2 vs py3 issues with encoding nonsense.  Skip for now.
         # tweak for brain damaged Nook STR.  Nook insists on name before content.
-        contentxml = unicode(contentxml).replace('<meta content="%s" name="cover"/>'%coverimgid,
-                                                 '<meta name="cover" content="%s"/>'%coverimgid)
+        # contentxml = contentxml.replace('<meta content="%s" name="cover"/>'%coverimgid,
+        #                                 '<meta name="cover" content="%s"/>'%coverimgid)
 
         
         outputepub.writestr("content.opf",contentxml)
