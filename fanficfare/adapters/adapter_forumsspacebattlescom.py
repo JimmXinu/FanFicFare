fa10cd36d15c1e5606175aa5d154a8c1c1c1ddb0
Jim Miller <retiefjimm@gmail.com>
2018-07-30 14:00:12 -0500

    p2/3 version of xenforoforum & SB

--- fanficfare/adapters/adapter_forumsspacebattlescom.py
+++ fanficfare/adapters/adapter_forumsspacebattlescom.py
@@ -1,6 +1,6 @@
 #  -*- coding: utf-8 -*-
 
-# Copyright 2015 FanFicFare team
+# Copyright 2018 FanFicFare team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,10 @@
 # limitations under the License.
 #
 
+from __future__ import absolute_import
 import re
 
-from base_xenforoforum_adapter import BaseXenForoForumAdapter
+from .base_xenforoforum_adapter import BaseXenForoForumAdapter
 
 def getClass():
     return ForumsSpacebattlesComAdapter
