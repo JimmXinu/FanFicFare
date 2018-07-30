58402ea6e5af25f6bde2ab771fd3772b165b8326
Jim Miller <retiefjimm@gmail.com>
2018-07-30 12:34:19 -0500

    Fix p2/p3 unichr

--- fanficfare/htmlcleanup.py
+++ fanficfare/htmlcleanup.py
@@ -24,6 +24,7 @@ import re
 # py2 vs py3 transition
 from six import text_type as unicode
 from six import string_types as basestring
+from six import unichr
 
 def _unirepl(match):
     "Return the unicode string for a decimal number"
