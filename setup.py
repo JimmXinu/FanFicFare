e5cafa496d70508b19b9c4ea6199d85f52594254
Jim Miller <retiefjimm@gmail.com>
2018-08-27 13:33:51 -0500

    Bump Test Version 2.37.14

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name=package_name,
 
     # Versions should comply with PEP440.
-    version="2.37.12",
+    version="2.37.14",
 
     description='A tool for downloading fanfiction to eBook formats',
     long_description=long_description,
