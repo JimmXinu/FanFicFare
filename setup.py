09fd06cd52f7e55b30de138b0472f0fd638d28d1
Jim Miller <retiefjimm@gmail.com>
2018-09-07 12:54:26 -0500

    Set pip install dev status: Production/Stable

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         'Environment :: Console',
 
