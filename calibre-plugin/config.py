949d1e73ea3c3f6d3f216d28e576b11d6756f936
Jim Miller <retiefjimm@gmail.com>
2018-12-25 23:36:36 -0600

    Force Translation Update

--- calibre-plugin/config.py
+++ calibre-plugin/config.py
@@ -79,7 +79,6 @@ no_trans = { 'pini':'personal.ini',
              'u':'username',
              'p':'password',
              }
-TRANS_TEMP = _('A Translation')
 
 STD_COLS_SKIP = ['size','cover','news','ondevice','path','series_sort','sort']
 
