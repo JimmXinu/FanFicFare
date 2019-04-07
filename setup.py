e0d83ce545fffcc6fa70daeeb53015d2f1cd1786
Jim Miller <retiefjimm@gmail.com>
2019-04-06 19:45:22 -0500

    Update a comment link in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """A setuptools based setup module.
 
 See:
-https://packaging.python.org/en/latest/distributing.html
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
