c8e7d4fbfa204558c9cf6e015fbdf05d3349c117
Jim Miller <retiefjimm@gmail.com>
2019-06-16 12:45:54 -0500

    Include soupsieve & backports in PI zip.

--- makeplugin.py
+++ makeplugin.py
@@ -35,7 +35,7 @@ if __name__=="__main__":
                   exclude=exclude)
 
     os.chdir('../included_dependencies')
-    files=['bs4','chardet','html2text']
+    files=['bs4','chardet','html2text','soupsieve','backports']
     # calibre has it's own copies of these that precedence anyway:
     # 'html5lib','webencodings'
     # webencodings is only needed by versions of html5lib after 0.9x7
