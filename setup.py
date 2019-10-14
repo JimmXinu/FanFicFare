400b81720f975f12a2525746b4e2b3dab13966be
Jim Miller <retiefjimm@gmail.com>
2019-10-14 10:23:36 -0500

    Bump Release Version 3.12.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name=package_name,
 
     # Versions should comply with PEP440.
-    version="3.11.9",
+    version="3.12.0",
 
     description='A tool for downloading fanfiction to eBook formats',
     long_description=long_description,
