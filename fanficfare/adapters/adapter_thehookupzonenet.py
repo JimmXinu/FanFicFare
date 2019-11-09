8d95a8bab2e4dbcf4869d01a0068b919d3154475
Jim Miller <retiefjimm@gmail.com>
2019-11-09 10:13:49 -0600

    Bypass expired SSL cert by not using SSL. adapter_thehookupzonenet

--- fanficfare/adapters/adapter_thehookupzonenet.py
+++ fanficfare/adapters/adapter_thehookupzonenet.py
@@ -21,12 +21,12 @@ from .base_efiction_adapter import BaseEfictionAdapter
 
 class TheHookupZoneNetAdapter(BaseEfictionAdapter):
 
-    @classmethod
-    def getProtocol(self):
-        """
-        Some, but not all site now require https.
-        """
-        return "https"
+    # @classmethod
+    # def getProtocol(self):
+    #     """
+    #     Some, but not all site now require https.
+    #     """
+    #     return "https"
 
     @staticmethod
     def getSiteDomain():
