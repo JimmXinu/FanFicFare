1669c067039ef867dfdf48ab9382a832ca58a9fc
Kolbo <kolbo5.git@kolbo5.org>
2020-07-05 11:15:33 +0200

    feat: add chireads.com adapter and tests on this one

--- fanficfare/adapters/__init__.py
+++ fanficfare/adapters/__init__.py
@@ -169,6 +169,7 @@ from . import adapter_fanficsme
 from . import adapter_archivehpfanfictalkcom
 from . import adapter_scifistoriescom
 from . import adapter_silmarillionwritersguildorg
+from . import adapter_chireadscom
 
 ## This bit of complexity allows adapters to be added by just adding
 ## importing.  It eliminates the long if/else clauses we used to need
