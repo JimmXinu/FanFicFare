1578a9f724b9c0a5301c4d8127205e91faf8ac37
ElminsterAU <github@thorsten.engler.id.au>
2020-09-17 01:44:18 +1000

    adds support for novelupdates.cc this is currently a 1:1 copy of the adapter for wuxiaworld.co with just the relevant strings replaced

--- fanficfare/adapters/__init__.py
+++ fanficfare/adapters/__init__.py
@@ -160,6 +160,7 @@ from . import adapter_wattpadcom
 from . import adapter_novelonlinefullcom
 from . import adapter_wwwnovelallcom
 from . import adapter_wuxiaworldco
+from . import adapter_novelupdatescc
 from . import adapter_harrypotterfanfictioncom
 from . import adapter_hentaifoundrycom
 from . import adapter_mugglenetfanfictioncom
