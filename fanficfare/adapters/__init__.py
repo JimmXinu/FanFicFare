f06ca006d0260a32c442c84f93864d28675b85a1
Jim Miller <retiefjimm@gmail.com>
2020-09-25 12:31:10 -0500

    New site: www.the-sietch.com XenForo2 site with weird URL scheme. Closes #430

--- fanficfare/adapters/__init__.py
+++ fanficfare/adapters/__init__.py
@@ -173,6 +173,7 @@ from . import adapter_chireadscom
 from . import adapter_scribblehubcom
 from . import adapter_fictionlive
 from . import adapter_wuxiaworldsite
+from . import adapter_thesietchcom
 
 ## This bit of complexity allows adapters to be added by just adding
 ## importing.  It eliminates the long if/else clauses we used to need
---
+++ fanficfare/adapters/adapter_thesietchcom.py
@@ -0,0 +1,53 @@
+#  -*- coding: utf-8 -*-
+
+# Copyright 2020 FanFicFare team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import absolute_import
+import re
+
+# py2 vs py3 transition
+from ..six import text_type as unicode
+
+from .base_xenforo2forum_adapter import BaseXenForo2ForumAdapter
+
+def getClass():
+    return TheSietchComAdapter
+
+class TheSietchComAdapter(BaseXenForo2ForumAdapter):
+
+    def __init__(self, config, url):
+        BaseXenForo2ForumAdapter.__init__(self, config, url)
+
+        # Each adapter needs to have a unique site abbreviation.
+        self.story.setMetadata('siteabbrev','sietch')
+
+    @staticmethod # must be @staticmethod, don't remove it.
+    def getSiteDomain():
+        # The site domain.  Does have www here, if it uses it.
+        return 'www.the-sietch.com'
+
+    @classmethod
+    def getPathPrefix(cls):
+        # in case it needs more than just site/
+        return '/index.php?'
+
+    def make_reader_url(self,tmcat_num,reader_page_num):
+        # https://www.the-sietch.com/index.php?threads/shattered-sphere-the-arcadian-free-march.3243/reader/page-2
+        # discard tmcat_num -- the-sietch.com doesn't have multiple
+        # threadmark categories yet.
+        return self.story.getMetadata('storyUrl')+'reader/page-'+unicode(reader_page_num)
+
+# XXX different threadmarks categories
