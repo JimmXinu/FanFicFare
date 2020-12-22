e9f933a7f7927a566f926d1e9787e7cb85e3194d
Jim Miller <retiefjimm@gmail.com>
2020-12-22 14:29:48 -0600

    Bump Test Version 3.26.6

--- fanficfare/cli.py
+++ fanficfare/cli.py
@@ -40,7 +40,7 @@ else: # > 3.0
     def pickle_load(f):
         return pickle.load(f,encoding="bytes")
 
-version="3.26.5"
+version="3.26.6"
 os.environ['CURRENT_VERSION_ID']=version
 
 global_cache = 'global_cache'
