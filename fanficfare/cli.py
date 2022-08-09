842b2d2d552b19a577b346c98136a692fda3340e
Jim Miller <retiefjimm@gmail.com>
2022-08-09 08:53:18 -0500

    Bump Test Version 4.14.9

--- fanficfare/cli.py
+++ fanficfare/cli.py
@@ -28,7 +28,7 @@ import string
 import os, sys, platform
 
 
-version="4.14.8"
+version="4.14.9"
 os.environ['CURRENT_VERSION_ID']=version
 
 global_cache = 'global_cache'
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name=package_name,
 
     # Versions should comply with PEP440.
-    version="4.14.8",
+    version="4.14.9",
 
     description='A tool for downloading fanfiction to eBook formats',
     long_description=long_description,
