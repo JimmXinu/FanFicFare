f4fbbf0d3402151cdd6ab6a520161fac7d37dc16
Jim Miller <retiefjimm@gmail.com>
2023-03-29 22:29:14 -0500

    Remove site: sword.borderline-angel.com broken ~3 years

--- fanficfare/adapters/__init__.py
+++ fanficfare/adapters/__init__.py
@@ -117,7 +117,6 @@ from . import adapter_asianfanficscom
 from . import adapter_mttjustoncenet
 from . import adapter_narutoficorg
 from . import adapter_starskyhutcharchivenet
-from . import adapter_swordborderlineangelcom
 from . import adapter_tasteofpoisoninkubationnet
 from . import adapter_thedelphicexpansecom
 from . import adapter_wwwaneroticstorycom
