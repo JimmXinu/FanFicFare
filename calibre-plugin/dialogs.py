22e0e8da66d1527b6dc827ef3404d691aec1d168
Jim Miller <retiefjimm@gmail.com>
2024-03-03 17:20:57 -0600

    Report errors during library update loop better.

--- calibre-plugin/dialogs.py
+++ calibre-plugin/dialogs.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 from datetime import datetime
+import traceback, sys
 
 from PyQt5 import QtWidgets as QtGui
 from PyQt5 import QtCore
@@ -37,6 +38,7 @@ except:
 from calibre.gui2 import gprefs
 show_download_options = 'fff:add new/update dialogs:show_download_options'
 from calibre.gui2.dialogs.confirm_delete import confirm
+from calibre.gui2 import error_dialog
 from calibre.gui2.complete2 import EditWithComplete
 from fanficfare.six import text_type as unicode, ensure_text
 
@@ -656,6 +658,7 @@ class _LoopProgressDialog(QProgressDialog):
         QProgressDialog.__init__(self,
                                  init_label,
                                  _('Cancel'), 0, len(book_list), gui)
+        self.gui = gui
         self.setWindowTitle(win_title)
         self.setMinimumWidth(500)
         self.book_list = book_list
@@ -723,6 +726,12 @@ class _LoopProgressDialog(QProgressDialog):
             book['status']=_("Error")
             book['comment']=unicode(e)
             logger.error("Exception: %s:%s"%(book,book['comment']),exc_info=True)
+            det_msg = "".join(traceback.format_exception(*sys.exc_info())) # +"\n"+_("Story Details:")+pretty_book(book)
+            error_dialog(self.gui,
+                         _("Error Updating Metadata"),
+                         "<p>"+_("An error has occurred while FanFicFare was updating calibre's metadata for <a href='%s'>%s</a>.")%(book['url'],book['title'])+"</p>",
+                         det_msg=det_msg,
+                         show=True)
 
         self.updateStatus()
         self.i += 1
