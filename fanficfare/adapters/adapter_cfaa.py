073696e0131e136dde6f636e146b5f4e68417b8b
xxwhirlpool <melodicake@disroot.org>
2024-12-30 15:10:32 -0500

    fixes slash changes for PR

--- fanficfare/adapters/adapter_cfaa.py
+++ fanficfare/adapters/adapter_cfaa.py
@@ -1,6 +1,6 @@
 #  -*- coding: utf-8 -*-
 
-# Copyright 2020 FanFicFare team
+# Copyright 2024 FanFicFare team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
