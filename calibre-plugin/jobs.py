JimmXinu/FanFicFare

commit 270e01c3c747e012cb85a6c17d495406b7c4b4f7

Cache config values for performance improvement.

fanficfare/configurable.py
@@ -606,6 +606,9 @@ class Configuration(ConfigParser):
 
         self.url_config_set = False
 
+        ## to improve performance, cache config values.
+        self.cached_config = {}
+
     def section_url_names(self,domain,section_url_f):
         ## domain is passed as a method to limit the damage if/when an
         ## adapter screws up _section_url
@@ -683,6 +686,10 @@ class Configuration(ConfigParser):
         return self.get_config(self.sectionslist,key,default)
 
     def get_config(self, sections, key, default=""):
+        try:
+            return self.cached_config[(tuple(sections),key)]
+        except KeyError as ke:
+            pass
         val = default
 
         val_files = []
@@ -727,6 +734,7 @@ class Configuration(ConfigParser):
             except (configparser.NoOptionError, configparser.NoSectionError) as e:
                 pass
 
+        self.cached_config[(tuple(sections),key)] = val
         return val
 
     # split and strip each.

commit dac6613a097f985e67dff8cb762278d735ee4669

Fix for PI's 'pass calibre columns into FanficFare' broken by 270e01c

calibre-plugin/jobs.py
@@ -395,10 +395,18 @@ def inject_cal_cols(book,story,configuration):
         extra_valid = []
         for k in book['calibre_columns'].keys():
             v = book['calibre_columns'][k]
-            story.setMetadata(k,v['val'])
             injectini.append('%s_label:%s'%(k,v['label']))
             extra_valid.append(k)
         if extra_valid: # if empty, there's nothing to add.
             injectini.append("add_to_extra_valid_entries:,"+','.join(extra_valid))
+
+            # extra_valid_entries was already populated, clear cache.
+            configuration.reset_cached_config()
             configuration.read_file(StringIO('\n'.join(injectini)))
-            #print("added:\n%s\n"%('\n'.join(injectini)))
+            # logger.debug("added:\n%s\n"%('\n'.join(injectini)))
+
+            # loop a second time to set values so extra_valid_entries
+            # is correct first.
+            for k in book['calibre_columns'].keys():
+                v = book['calibre_columns'][k]
+                story.setMetadata(k,v['val'])

fanficfare/configurable.py
@@ -613,6 +613,12 @@ class Configuration(ConfigParser):
         self.url_config_set = False
 
         ## to improve performance, cache config values.
+        self.reset_cached_config()
+
+    def reset_cached_config(self):
+        ## should argubly be called by read_file, etc, but there's
+        ## several read methods.  Revisit if ever needed from more
+        ## than just calibre-plugin/jobs.py:inject_cal_cols()
         self.cached_config = {}
 
     def section_url_names(self,domain,section_url_f):