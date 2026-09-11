9dcf2b9fb49cdd55531d56b469d46afe43a30191
Jim Miller <retiefjimm@gmail.com>
2026-09-11 16:03:58 -0500

    Bump Test Version 4.61.3

--- calibre-plugin/__init__.py
+++ calibre-plugin/__init__.py
@@ -30,7 +30,7 @@ except NameError:
 from calibre.customize import InterfaceActionBase
 
 # pulled out from FanFicFareBase for saving in prefs.py
-__version__ = (4, 61, 2)
+__version__ = (4, 61, 3)
 
 ## Apparently the name for this class doesn't matter--it was still
 ## 'demo' for the first few versions.
